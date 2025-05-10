2bae6a29054dbc43278c48a01e17d94358c216d2
nycgithub156 <nycollasf156@gmail.com>
2025-05-09 22:16:57 -0300

    mensagem descritiva

---
+++ PetShop/PetClass.java
@@ -0,0 +1,45 @@
+public class PetClass {
+    private String name;
+    private String specie;
+    private String breed;
+    private int age;
+
+    public PetClass(String name, String specie, String breed, int age) {
+        this.name = name;
+        this.specie = specie;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecie() {
+        return specie;
+    }
+
+    public void setSpecie(String specie) {
+        this.specie = specie;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
